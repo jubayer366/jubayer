e60832d7dc8c2169a40ccd1d6682fa23ac1235e7
jubayer366 <62174089+jubayer366@users.noreply.github.com>
2020-03-14 17:25:26 +0600

    Add files via upload

---
+++ rifat eee.c
@@ -0,0 +1,16 @@
+#include<stdio.h>;
+
+//int SUM()
+int main()
+{
+     sum();
+}
+int sum()
+{
+     int a,b,s;
+     scanf("%d%d",&a,&b);
+     s=a+b;
+     printf ("%d\n",s);
+     return 0;
+}
+
